c6c6dfce965d27ca43dc53ab9e8d6a53aeaca383
Couchchip <jacobschneider999@gmail.com>
2022-10-27 19:32:10 -0700

    flashlight added to character

---
+++ Mansion_Horror/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    [SerializeField] private GameObject light1;
+    [SerializeField] private GameObject light2;
+
+    [SerializeField] private bool isOn = false;
+    //if character isn't holding, change to false
+    public bool characterIsHolding;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OnOff();
+    }
+
+    void OnOff()
+    {
+        if (Input.GetKeyDown(KeyCode.T) && !isOn)
+        {
+            isOn = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.T) && isOn)
+        {
+            isOn = false;
+        }
+
+        if (isOn)
+        {
+            light1.SetActive(true);
+            light2.SetActive(true);
+        }
+        if (!isOn)
+        {
+            light1.SetActive(false);
+            light2.SetActive(false);
+        }
+    }
+}
