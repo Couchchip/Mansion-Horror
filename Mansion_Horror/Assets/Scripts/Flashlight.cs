b9f5e957afd106707c359c94ccd85fed00bcfc0f
Couchchip <jacobschneider999@gmail.com>
2022-10-27 20:31:52 -0700

    can reload battery

--- Mansion_Horror/Assets/Scripts/Flashlight.cs
+++ Mansion_Horror/Assets/Scripts/Flashlight.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Flashlight : MonoBehaviour
 {
     [SerializeField] private GameObject light1;
 
+    [SerializeField] private float maxBattery;
+    [SerializeField] private float currentBattery;
+
+    [SerializeField] private bool canTurnOn;
     [SerializeField] private bool isOn = false;
     //if character isn't holding, change to false
     public bool characterIsHolding;
@@ -13,18 +18,26 @@ public class Flashlight : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentBattery = maxBattery;
+        canTurnOn = true;
     }
 
     // Update is called once per frame
     void Update()
     {
         OnOff();
+
+        if (Input.GetKeyDown(KeyCode.R) && currentBattery < maxBattery && !isOn)
+        {
+            StartCoroutine(RechargeBattery());
+            StopCoroutine(RechargeBattery());
+        }
     }
 
     void OnOff()
     {
-        if (Input.GetKeyDown(KeyCode.T) && !isOn)
+        //t to turn on/off with logic
+        if (Input.GetKeyDown(KeyCode.T) && !isOn && canTurnOn)
         {
             isOn = true;
         }
@@ -33,13 +46,25 @@ public class Flashlight : MonoBehaviour
             isOn = false;
         }
 
+        //on off
         if (isOn)
-        {
-            light1.SetActive(true);
-        }
+        { light1.SetActive(true); currentBattery = currentBattery - 1 * Time.deltaTime; }
         if (!isOn)
+        { light1.SetActive(false); }
+
+        //no battery means no work
+        if (currentBattery <= 0)
         {
-            light1.SetActive(false);
+            isOn = false;
+            canTurnOn = false;
         }
+
     }
+
+    IEnumerator RechargeBattery()
+    {
+        currentBattery = currentBattery + .5f;
+        yield return new WaitForSeconds(.5f);
+    }
+
 }
